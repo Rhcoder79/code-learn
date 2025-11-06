37fd8c8b7629379f9a14f800654dba8e74018f68
Rhcoder79 <robiulhasan7972171@gmail.com>
2025-11-06 18:19:30 +0600

    object task

---
+++ js-string-object/declare.js
@@ -0,0 +1,6 @@
+const pen={brand:'matador',price:10,color:'yellow'};
+console.log(pen);
+const pencil =new Object(price=7,color='red', location='badda');
+console.log(pencil);
+const rubber=Object.create({});
+console.log(rubber);
---
+++ js-string-object/string.js
@@ -0,0 +1,14 @@
+
+const division='chadpur';
+const thana=`hajigong`;
+const country= new String('bangladesh');
+console.log(typeof country);
+console.log( country);
+const capital='dha      ka';
+console.log(capital[7]);
+console.log(capital.length); 
+const number=[54,98,33,44,77,33];
+console.log( number);
+number[2]=52;
+console.log(number);
+
