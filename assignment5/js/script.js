4d74105567a6f24a07b31f78b2c3c0b1fd29d035
Rhcoder79 <robiulhasan7972171@gmail.com>
2025-11-27 17:47:19 +0600

    heart icon complete

---
+++ assignment5/js/script.js
@@ -0,0 +1,91 @@
+console.log('js file connected')
+document.getElementById("heart1")
+.addEventListener('click',function(e){
+    e.preventDefault()
+    const heartNum=document.getElementById("increase")
+     const grow=heartNum.innerText
+    const convert=parseInt(grow)
+    const totalHeart=convert+1;
+   heartNum.innerText=totalHeart;
+   
+})
+document.getElementById("heart2")
+.addEventListener('click',function(e){
+    e.preventDefault()
+    const heartNum=document.getElementById("increase")
+     const grow=heartNum.innerText
+    const convert=parseInt(grow)
+    const totalHeart=convert+1;
+   heartNum.innerText=totalHeart;
+   
+})
+document.getElementById("heart3")
+.addEventListener('click',function(e){
+    e.preventDefault()
+    const heartNum=document.getElementById("increase")
+     const grow=heartNum.innerText
+    const convert=parseInt(grow)
+    const totalHeart=convert+1;
+   heartNum.innerText=totalHeart;
+   
+})
+document.getElementById("heart4")
+.addEventListener('click',function(e){
+    e.preventDefault()
+    const heartNum=document.getElementById("increase")
+     const grow=heartNum.innerText
+    const convert=parseInt(grow)
+    const totalHeart=convert+1;
+   heartNum.innerText=totalHeart;
+   
+})
+document.getElementById("heart5")
+.addEventListener('click',function(e){
+    e.preventDefault()
+    const heartNum=document.getElementById("increase")
+     const grow=heartNum.innerText
+    const convert=parseInt(grow)
+    const totalHeart=convert+1;
+   heartNum.innerText=totalHeart;
+   
+})
+document.getElementById("heart6")
+.addEventListener('click',function(e){
+    e.preventDefault()
+    const heartNum=document.getElementById("increase")
+     const grow=heartNum.innerText
+    const convert=parseInt(grow)
+    const totalHeart=convert+1;
+   heartNum.innerText=totalHeart;
+   
+})
+document.getElementById("heart7")
+.addEventListener('click',function(e){
+    e.preventDefault()
+    const heartNum=document.getElementById("increase")
+     const grow=heartNum.innerText
+    const convert=parseInt(grow)
+    const totalHeart=convert+1;
+   heartNum.innerText=totalHeart;
+   
+})
+document.getElementById("heart8")
+.addEventListener('click',function(e){
+    e.preventDefault()
+    const heartNum=document.getElementById("increase")
+     const grow=heartNum.innerText
+    const convert=parseInt(grow)
+    const totalHeart=convert+1;
+   heartNum.innerText=totalHeart;
+   
+})
+document.getElementById("heart9")
+.addEventListener('click',function(e){
+    e.preventDefault()
+    const heartNum=document.getElementById("increase")
+     const grow=heartNum.innerText
+    const convert=parseInt(grow)
+    const totalHeart=convert+1;
+   heartNum.innerText=totalHeart;
+   
+})
